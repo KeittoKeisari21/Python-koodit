7cb4dadbcb76a784a63eb6ff5f5c60cf00a2c42b
KeittoKeisari21 <filipvojnovic0@gmail.com>
2024-08-21 15:01:19 +0300

    moro

---
+++ Moduuli 2/tehtava_6.py
@@ -0,0 +1,13 @@
+import random
+
+ek =  ""
+tk = ""
+for x in range(3):
+    ek = ek + str(random.randint(0,9))
+print(ek)
+
+
+
+for x in range(4):
+    tk = tk + str(random.randint(1,6))
+print(tk)
